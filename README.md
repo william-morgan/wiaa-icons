aaa2de5a204b2bf502928578a7e4e3362e0ee696
William Morgan <william.morgan@uwalumni.com>
2015-09-24 17:48:08 -0500

    G-E-T fix

---
+++ README.md
@@ -0,0 +1,6 @@
+A plugin that adds a wiaa icon  emoji set into a discourse install.
+
+License:
+
+All Ruby and Javascript code are MIT
+
---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: wiaa_emoji
+# about: adds wiaa emojis to discourse install
+# version: 0.1
+# authors: ttalk
+
+register_asset "javascripts/wiaa-emoji.js", :server_side
